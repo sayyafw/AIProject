bc8b9b27c44937a6705a1d45bb1a17a99d065f62
Faan1207 <andrewsongda@gmail.com>
2018-03-30 05:40:18 +1100

    Check before submission

--- A_Star.py
+++ A_Star.py
@@ -8,13 +8,14 @@ class AStarSearch:
         self.board_width = board_width
         self.board_length = board_length
 
+    #calculate the manhattan distance between 2 co-ords
     @staticmethod
     def manhattan_distance(x1, y1, x2, y2):
         dist_x = fabs(x1 - x2)
         dist_y = fabs(y1 - y2)
         return dist_x + dist_y
 
-    # Issue in getSquare for move3 in massacre mode
+    #get the targetted square object
     def get_square(self, x, y, squares):
         return squares[x * self.board_width + y]
 
@@ -23,37 +24,52 @@ class AStarSearch:
 
         list_of_squares = []
 
+        #if the square is not on the edge
         if square.x > 0:
+            #get the square on the left of this square
             adj_left = self.get_square(square.x - 1, square.y, squares)
+            #if the square on the left isn't blocked, take it.
             if not self.blocked(adj_left, squares):
                 list_of_squares.append(adj_left)
+            #elsewise, check if it can be jumped over
+            elif 1 < square.x < 6 and not self.blocked(self.get_square\
+                (square.x - 2, square.y, squares), squares):
+                #it can be jumped over, take this square
+                list_of_squares.append(self.get_square(square.x - 2, square.y, 
+                    squares))
 
-            elif 1 < square.x < 6 and not self.blocked(self.get_square(square.x - 2, square.y, squares), squares):
-                list_of_squares.append(self.get_square(square.x - 2, square.y, squares))
-
+        #repetiton of the above, but for the up side.
         if square.y > 0:
             adj_up = self.get_square(square.x, square.y - 1, squares)
             if not self.blocked(adj_up, squares):
                 list_of_squares.append(adj_up)
 
-            elif 1 < square.y < 6 and not self.blocked(self.get_square(square.x, square.y - 2, squares), squares):
-                list_of_squares.append(self.get_square(square.x, square.y - 2, squares))
+            elif 1 < square.y < 6 and not self.blocked(self.get_square(square.x,
+             square.y - 2, squares), squares):
+                list_of_squares.append(self.get_square(square.x, square.y - 2, 
+                    squares))
 
+        #repetition of the above, but for the rigt side.
         if square.x < self.board_width - 1:
             adj_right = self.get_square(square.x + 1, square.y, squares)
             if not self.blocked(adj_right, squares):
                 list_of_squares.append(adj_right)
 
-            elif 1 < square.x < 6 and not self.blocked(self.get_square(square.x + 2, square.y, squares), squares):
-                list_of_squares.append(self.get_square(square.x + 2, square.y, squares))
+            elif 1 < square.x < 6 and not self.blocked(self.get_square \
+                (square.x + 2, square.y, squares), squares):
+                list_of_squares.append(self.get_square(square.x + 2, square.y, 
+                    squares))
 
+        #repitition of the above, but for the down side.
         if square.y < self.board_width - 1:
             adj_down = self.get_square(square.x, square.y + 1, squares)
             if not self.blocked(adj_down, squares):
                 list_of_squares.append(adj_down)
 
-            elif 1 < square.x < 6 and not self.blocked(self.get_square(square.x, square.y + 2, squares),squares):
-                list_of_squares.append(self.get_square(square.x, square.y + 2, squares))
+            elif 1 < square.x < 6 and not self.blocked(self.get_square\
+                (square.x, square.y + 2, squares),squares):
+                list_of_squares.append(self.get_square(square.x, square.y + 2,
+                 squares))
 
         return list_of_squares
 
@@ -63,24 +79,32 @@ class AStarSearch:
         current_square = square
         path = []
 
-        while current_square.x != start_goal.x or current_square.y != start_goal.y:
-            coods = (current_square.x, current_square.y)
+        #while there are path to be print
+        while current_square.x != start_goal.x or \
+        current_square.y != start_goal.y:
+            #get both co-ords, and print from parent node to child.
+            coords = (current_square.x, current_square.y)
             parent_square = current_square.parent
-            parent_coods = (parent_square.x, parent_square.y)
+            parent_coords = (parent_square.x, parent_square.y)
             current_square = current_square.parent
-            path.append(str(parent_coods) + " -> " + str(coods))
+            #append the path to an array 
+            path.append(str(parent_coords) + " -> " + str(coords))
 
         i = len(path) - 1
 
+        #now print it out in reverse order.
         while i >= 0:
             print(path[i])
             i -= 1
 
     # Updates position when move is to an adjacent element
-    def update_position_adjacent(self, current_square, adjacent_square, goal_square):
+    def update_position_adjacent(self, current_square, \
+        adjacent_square, goal_square):
 
+        #-------------------------------------------------------------------------------
         adjacent_square.g = current_square.g + 1
-        adjacent_square.h = self.manhattan_distance(adjacent_square.x, adjacent_square.y, goal_square.x, goal_square.y)
+        adjacent_square.h = self.manhattan_distance(adjacent_square.x, 
+            adjacent_square.y, goal_square.x, goal_square.y)
         adjacent_square.f = adjacent_square.h + adjacent_square.g
         adjacent_square.parent = current_square
 
@@ -97,13 +121,14 @@ class AStarSearch:
         # keeps a copy of the start square
         start_square = copy.copy(current_square)
 
+        #while there are unvisited nodes.
         while len(unvisited):
             # Get first element from heap
             f, square = heapq.heappop(unvisited)
             # Show that this element has been visited
             visited.append(square)
 
-            # Check for goal state
+            # Check if we're already at the goal
             if self.state(square, goal_square):
                 end_square = square
                 self.print_moves(square, start_square)
@@ -114,30 +139,35 @@ class AStarSearch:
 
             # Checks each of the adjacent squares for the best move
             for i in range(len(adjacent_squares)):
+                #for each of the adjacent tiles
                 item = adjacent_squares[i]
+                #if tile is not visited and not blocked
                 if item not in visited and  not self.blocked(item, squares):
+                    #if ---------------------------------------------------------------
                     if (item.f, item) in unvisited:
-
-                        # If adj cell is open, check if current
-                        # path is better than
-                        # previously recorded path for this cell
+                        #check if current path is better than the previous one
                         if item.g < square.g + 1:
-                            self.update_position_adjacent(square, item, goal_square)
-
+                            self.update_position_adjacent(square, item, 
+                                goal_square)
                     else:
+                        #update the adjacent positions
                         self.update_position_adjacent(square, item, goal_square)
+                        #------------------------------------------------------------
                         heapq.heappush(unvisited, (item.f, item))
 
         return end_square
 
+    #check if given tile is blocked
     def blocked(self, square, squares):
+        #if the tile is not empty, it's blocked
         if squares[square.x * self.board_width + square.y].value != "-":
             return True
         return False
 
-    # Check for goal state
+    #Check if the 2 squares are the same
     @staticmethod
     def state(current_square, goal_square):
+        #if they're the same, return true
         if current_square.x == goal_square.x and \
                 current_square.y == goal_square.y:
             return True
@@ -148,14 +178,16 @@ class AStarSearch:
     @staticmethod
     def check_jump(square, board, direction):
 
+        #check for edges
         if (square.x > 6 or square.x < 1) and (square.y > 6 or square.y < 1):
             return False
+
+        #check for every direction, True if  ---------------------------------------------
         if direction == 0:
             if square.x > 0 and board[square.y][square.x - 1] == "-":
                 return True
         if direction == 1:
             if square.y > 0 and board[square.y - 1][square.x] == "-":
-                print(True)
                 return True
         if direction == 2:
             if square.x < 7 and board[square.y][square.x + 1] == "-":
--- Board.py
+++ Board.py
@@ -22,11 +22,12 @@ class Board:
     # Populate board with input string
     def populate_board(self):
         for i in range(0, 8):
+            #for each row is split into and array
             self.board.append(input())
             self.board[i] = self.board[i].split()
         self.mode = input()
 
-    # print board in list form
+    # print board in readable form
     def print_board(self):
         for item in self.board:
             print(item)
@@ -34,77 +35,83 @@ class Board:
     # find where the black and white pieces are on the board
     def read_board(self):
         board = self.board
+        #print an "empty" line for formatting 
         print("   ")
         self.print_board()
 
-        # Read the rows and columns of the board. Add each square to squares list. If white or black piece, add to
-        # requisite list
+        #for each row on the board
         for i in range(len(board)):
+            #for each column in the row
             for j in range(len(board[i])):
+                #add this to the list of squares
                 self.squares.append(Square(i, j, board[j][i]))
+                #and add an piece corresponded to its' colour
                 if board[i][j] == "O":
                     self.white_pieces.append(WhitePiece(j, i))
 
                 elif board[i][j] == "@":
                     self.black_pieces.append(BlackPiece(j, i))
 
-    # find possible moves for white and black pieces
+    # find amount of possible moves for white and black pieces
     def move(self):
         total_moves_white = 0
         total_moves_black = 0
 
         for white_item in self.white_pieces:
-            total_moves_white += self.check_surroundings(white_item.x, white_item.y)
+            total_moves_white += self.surrounding(white_item.x, white_item.y)
 
         print(total_moves_white)
 
         for black_item in self.black_pieces:
-            total_moves_black += self.check_surroundings(black_item.x, black_item.y)
+            total_moves_black += self.surrounding(black_item.x, black_item.y)
 
         print(total_moves_black)
 
-    # check surrounding blocks for available moves. For each impossible move found, subtract one possible move
-    def check_surroundings(self, x, y):
+    # check surrounding blocks and count available moves
+    def surrounding(self, x, y):
         board = self.board
+        #maximum 4 possible moves, substract for each impossible move
         total_moves = 4
 
+        #check if x, y is on the edge of the board.
         if x == 0 or x == 7:
             total_moves -= 1
 
         if y == 0 or y == 7:
             total_moves -= 1
 
+        #if x is within boundary of the board.
         if 1 <= x <= 6:
+            #if to the right is not empty
             if board[y][x + 1] != "-":
-                # If this happens, means square cannot be jumped over or moved onto
+                #then check if it can be jumped over
                 if x > 5 or board[y][x + 2] != "-":
+                    #cannot be jumped over, found an impossible move
                     total_moves -= 1
-
+            #same as above, but for a different axis
             if board[y][x - 1] != "-":
-                # If this happens, means square cannot be jumped over or moved onto
                 if x < 2 or board[y][x - 2] != "-":
                     total_moves -= 1
 
         if 1 <= y <= 6:
-
+            #same as above, but for a different axis
             if board[y + 1][x] != "-":
-                # If this happens, means square cannot be jumped over or moved onto
                 if y > 5 or board[y + 2][x] != "-":
                     total_moves -= 1
-
+            #same as above, but for a different axis
             if board[y - 1][x] != "-":
-                # If this happens, means square cannot be jumped over or moved onto
                 if y < 2 or board[y - 2][x] != "-":
                     total_moves -= 1
 
         return total_moves
 
-    # Finds the two closest white pieces to a given coordinate. Smallest possible distance kept at min_dist_1
+    # Finds the two closest white pieces to a given coordinate.
     def closest_piece(self, x, y):
         white_pieces = self.white_pieces
 
-        if AStarSearch.manhattan_distance(x, y, white_pieces[0].x, white_pieces[0].y) > \
-                AStarSearch.manhattan_distance(x, y, white_pieces[1].x, white_pieces[1].y):
+        if AStarSearch.manhattan_distance(x, y, white_pieces[0].x, 
+            white_pieces[0].y) > AStarSearch.manhattan_distance(x, y, 
+            white_pieces[1].x, white_pieces[1].y):
 
             min_dist_1 = white_pieces[1]
             min_dist_2 = white_pieces[0]
@@ -116,9 +123,12 @@ class Board:
         i = 2
 
         while i < len(self.white_pieces):
-            manhattan_dist = AStarSearch.manhattan_distance(x, y, white_pieces[i].x, white_pieces[i].y)
-            if manhattan_dist < AStarSearch.manhattan_distance(x, y, min_dist_2.x, min_dist_2.y):
-                if manhattan_dist < AStarSearch.manhattan_distance(x, y, min_dist_1.x, min_dist_1.y):
+            manhattan_dist = AStarSearch.manhattan_distance(x, y, 
+                white_pieces[i].x, white_pieces[i].y)
+            if manhattan_dist < AStarSearch.manhattan_distance(x, y, 
+                min_dist_2.x, min_dist_2.y):
+                if manhattan_dist < AStarSearch.manhattan_distance(x, y, 
+                    min_dist_1.x, min_dist_1.y):
                     min_dist_2 = copy.copy(min_dist_1)
                     min_dist_1 = white_pieces[i]
 
@@ -130,114 +140,135 @@ class Board:
 
     # Method for massacre mode
     def massacre(self):
-
+        #iterate through every black piece on the board
         for item in self.black_pieces:
-
-            available_coods = self.check_takeable(item)
-
-            if available_coods == [[], []]:
+            #find the positions that can eat this piece
+            available_coords = self.check_takeable(item)
+            #------------------------------------------------------------------------------
+            if available_coords == [[], []]:
                 continue
-            x_dir = available_coods[0]
-            y_dir = available_coods[1]
+            #get the 2 pair of positions
+            x_dir = available_coords[0]
+            y_dir = available_coords[1]
 
+            #determine which 2 positions takes less move to eat the black piece
             best_dir, piece_1, piece_2 = self.choose_best_dir(x_dir, y_dir)
             # initialises A* Search Class
             a_star_algo = AStarSearch(board_width, board_length)
             # keeps copy of current pieces original location
             old_x, old_y = piece_1.x, piece_1.y
+
             # Does A* search
-            new_location = a_star_algo.search(self.squares[best_dir[1] * board_width + best_dir[0]],
-                                                self.squares[piece_1.x * board_width + piece_1.y], self.squares)
+            new_location = a_star_algo.search(self.squares[best_dir[1] * \
+                board_width + best_dir[0]], self.squares[piece_1.x * \
+                board_width + piece_1.y], self.squares)
             # Updates board
             self.update_pieces(new_location, old_x, old_y)
             # keeps copy of current pieces original location
             old_x, old_y = piece_2.x, piece_2.y
-            new_location = a_star_algo.search(self.squares[best_dir[3] * board_width + best_dir[2]],
-                                                self.squares[piece_2.x * board_width + piece_2.y], self.squares)
+
+            # Do another A* search
+            new_location = a_star_algo.search(self.squares[best_dir[3] * \
+                board_width + best_dir[2]], self.squares[piece_2.x * \
+                board_width + piece_2.y], self.squares)
+            #-------------------------------------------------------------------------------
             if new_location is None:
                 self.squares[item.x * board_width + item.y].value = "-"
-                print("END")
                 continue
 
+            #update the pieces after each set of moves
             self.update_pieces(new_location, old_x, old_y)
             # removes dead black piece
 
-            print("END")
-
     def update_pieces(self, new_location, old_x, old_y):
-
+        #the new location now is marked with "O"
         self.squares[new_location.x * board_width + new_location.y].value = "O"
+        #iterate through all the white pieces
         for item in self.white_pieces:
+            #if the piece's co-ord is the same as the old location, update it
             if item.x == old_x and item.y == old_y:
                 item.x = new_location.x
                 item.y = new_location.y
-
+        #---------------------------------------------------------------------------------
         if old_x != new_location.x and old_x != new_location.x:
             self.squares[old_x * board_width + old_y].value = "-"
 
     # Check which of the two positions are available for taking the piece
     def check_takeable(self, bp):
         squares = self.squares
-        available_coods = [[], []]
+        available_coords = [[], []]
 
         # Check if takeable in x direction
         if 1 <= bp.x <= 6:
             # Check if both spaces in x direction free
             if squares[(bp.x-1) * board_width + bp.y].value == "-" and \
                     squares[(bp.x + 1) * board_width + bp.y].value == "-":
-                available_coods[0] = [bp.y, bp.x - 1, bp.y, bp.x + 1]
+                available_coords[0] = [bp.y, bp.x - 1, bp.y, bp.x + 1]
 
+            #Check if either space in x direction is free.
             elif squares[(bp.x-1) * board_width + bp.y].value == "O" and \
                     squares[(bp.x + 1) * board_width + bp.y].value == "-":
-                available_coods[0] = [bp.y, bp.x - 1, bp.y, bp.x + 1]
+                available_coords[0] = [bp.y, bp.x - 1, bp.y, bp.x + 1]
 
             elif squares[(bp.x-1) * board_width + bp.y].value == "-" and \
                     squares[(bp.x + 1) * board_width + bp.y].value == "O":
-                available_coods[0] = [bp.y, bp.x - 1, bp.y, bp.x + 1]
+                available_coords[0] = [bp.y, bp.x - 1, bp.y, bp.x + 1]
 
         # Check if takeable in y direction
         if 1 <= bp.y <= 6:
             # Check if both spaces in y direction free
             if squares[bp.x * board_width + (bp.y - 1)].value == "-" and \
                     squares[bp.x * board_width + (bp.y + 1)].value == "-":
-                available_coods[1] = [bp.y - 1, bp.x, bp.y + 1, bp.x]
+                available_coords[1] = [bp.y - 1, bp.x, bp.y + 1, bp.x]
 
-            # Check if one space already covered by white piece and other free
+            # Check if eithr space in y direction is free
             elif squares[bp.x * board_width + (bp.y - 1)].value == "O" and \
                     squares[bp.x * board_width + (bp.y + 1)].value == "-":
-                available_coods[1] = [bp.y - 1, bp.x, bp.y + 1, bp.x]
+                available_coords[1] = [bp.y - 1, bp.x, bp.y + 1, bp.x]
 
             elif squares[bp.x * board_width + (bp.y - 1)].value == "-" and \
                     squares[bp.x * board_width + (bp.y + 1)].value == "O":
-                available_coods[1] = [bp.y - 1, bp.x, bp.y + 1, bp.x]
+                available_coords[1] = [bp.y - 1, bp.x, bp.y + 1, bp.x]
 
-        return available_coods
+        return available_coords
 
+    #given 2 pairs of squares, return the one with least movements.
     def choose_best_dir(self, x_dir, y_dir):
 
+        #if neither are present.
         if not x_dir and not y_dir:
             return None
 
+        #if x_dir is non-existant.
         elif not x_dir:
             piece3, piece4 = self.closest_piece(y_dir[1], y_dir[0])
             return y_dir, piece3, piece4
 
+        #if y_dir is non-existant
         elif not y_dir:
             piece1, piece2 = self.closest_piece(x_dir[1], x_dir[0])
             return x_dir, piece1, piece2
 
+        #---------------------------------------------------------------------------------
         piece1, piece2 = self.closest_piece(x_dir[1], x_dir[0])
 
-        x_dist_1 = AStarSearch.manhattan_distance(x_dir[1], piece1.x, x_dir[0], piece1.y)
-        x_dist_2 = AStarSearch.manhattan_distance(x_dir[3], piece2.x, x_dir[2], piece2.y)
+        #calculate the distances of each piece to a destination
+        x_dist_1 = AStarSearch.manhattan_distance(x_dir[1], piece1.x, 
+            x_dir[0], piece1.y)
+        x_dist_2 = AStarSearch.manhattan_distance(x_dir[3], piece2.x, 
+            x_dir[2], piece2.y)
 
+        #-----------------------------------------------------------------------------------
         piece3, piece4 = self.closest_piece(y_dir[1], y_dir[0])
 
-        y_dist_1 = AStarSearch.manhattan_distance(y_dir[1], piece3.x, y_dir[0], piece3.y)
-        y_dist_2 = AStarSearch.manhattan_distance(y_dir[3], piece4.x, y_dir[2], piece4.y)
+        #calculate the distance of each piece to a destination
+        y_dist_1 = AStarSearch.manhattan_distance(y_dir[1], piece3.x, 
+            y_dir[0], piece3.y)
+        y_dist_2 = AStarSearch.manhattan_distance(y_dir[3], piece4.x, 
+            y_dir[2], piece4.y)
 
+        #return the better value.
         if x_dist_1 + x_dist_2 <= y_dist_1 + y_dist_2:
-
             return x_dir, piece1, piece2
 
         else:
